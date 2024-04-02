814677c9339d51769f897b2350eb9fb9f58f08eb
dmn8xt <dmn8xt@gmail.com>
2024-04-02 11:31:15 +0300

    update second.js

--- second.js
+++ second.js
@@ -1,1 +1,14 @@
-console.log('second js files')
+setTimeout(function timeout() {
+    console.log('T')
+  }, 0)
+  
+  var p = new Promise(function(resolve, reject) {
+    console.log('F');
+    resolve()
+  })
+  
+  p.then(function() {
+    console.log('L')
+  })
+  
+  console.log('G')
