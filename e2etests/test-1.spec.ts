3101a8849cada41dd7f7264bb19920609ad58052
dmn8xt <dmn8xt@gmail.com>
2024-04-04 13:59:42 +0300

    2 tests add

---
+++ e2etests/test-1.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://playwrightshop.ru/shop/');
+  await page.getByLabel('Добавить в корзину “Cap”').click();
+  await page.getByLabel('Добавить в корзину “Polo”').click();
+  await page.getByLabel('В корзине 2 товара').click();
+  await page.getByRole('link', { name: 'Просмотр корзины' }).click();
+  await page.getByRole('link', { name: 'Перейти к оформлению заказа' }).click();
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('teste@test.ru');
+  await page.getByLabel('Имя').click();
+  await page.getByLabel('Имя').fill('Dm');
+  await page.getByText('Фамилия').click();
+  await page.getByLabel('Фамилия').fill('Bel');
+  await page.getByLabel('Адрес',{ exact: true }).click();
+  await page.getByLabel('Адрес', { exact: true }).fill('Центр ');
+  await page.getByLabel('США, Страна/регион').click();
+  await page.getByLabel('США, Страна/регион').fill('росс');
+  await page.getByLabel('Россия', { exact: true }).click();
+  await page.getByLabel('Город').click();
+  await page.getByLabel('Город').fill('Анапа');
+  await page.getByLabel('Область/район').click();
+  await page.getByLabel('Область/район').fill('анапский');
+  await page.getByLabel('Почтовый индекс').fill('111111');
+  await page.getByLabel('Телефон (дополнительно)').fill('12345678');
+  await page.getByRole('button', { name: 'Размещение заказа' }).click();
+  await page.getByRole('button', { name: 'Размещение заказа' }).click();
+  await page.getByText('Ваш заказ принят. Благодарим вас').click();
+});
---
+++ e2etests/test-2.spec.ts
@@ -0,0 +1,28 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://playwrightshop.ru/shop/');
+  await page.getByRole('link', { name: 'Product on sale Beanie' }).click();
+  await page.getByRole('button', { name: 'В корзину', exact: true }).click();
+  await page.getByRole('link', { name: 'Оформление заказа' }).click();
+  await page.getByLabel('Email').click();
+  await page.getByLabel('Email').fill('test@test.ru');
+  await page.getByLabel('Имя').click();
+  await page.getByLabel('Имя').fill('dm');
+  await page.getByLabel('Фамилия').click();
+  await page.getByLabel('Фамилия').fill('bel');
+  await page.getByLabel('Адрес', { exact: true }).click();
+  await page.getByLabel('Адрес', { exact: true }).fill('центр');
+  await page.getByLabel('США, Страна/регион').click();
+  await page.getByLabel('США, Страна/регион').fill('росси');
+  await page.getByText('Росси', { exact: true }).click();
+  await page.getByLabel('Город').click();
+  await page.getByLabel('Город').fill('анапа');
+  await page.getByLabel('Область/район').click();
+  await page.getByLabel('Область/район').fill('анапский');
+  await page.getByText('Почтовый индекс', { exact: true }).click();
+  await page.getByLabel('Почтовый индекс').fill('111111');
+  await page.getByRole('button', { name: 'Размещение заказа' }).click();
+  await page.getByRole('button', { name: 'Размещение заказа' }).click();
+  await page.getByText('Ваш заказ принят. Благодарим вас').click();
+});
