c67fef944968ede3148ed805b83304500d831a30
dmn8xt <dmn8xt@gmail.com>
2024-04-05 23:01:22 +0300

    del lines

--- e2etests/miniCartCount.spec.ts
+++ e2etests/miniCartCount.spec.ts
@@ -6,7 +6,6 @@ test('test', async ({ page }) => {
   await page.getByLabel('Добавить в корзину “Cap”').click();
   await page.getByLabel('Добавить в корзину “Polo”').click();
 
-  // wc-block-mini-cart__badge
-  // getByLabel('В корзине 0 товаров')
+  
   await expect(page.getByLabel('В корзине 2 товара')).toHaveText('2') 
 });
